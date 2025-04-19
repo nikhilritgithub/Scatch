5d3a4aef6b69a47e5b2fbb034725007cabb97b94
Nikhil kumar <142686565+nikhilritgithub@users.noreply.github.com>
2025-04-19 11:13:31 +0530

    Update productsRouter.js

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function(req, res){
-    res.send("hey it's working");
+    res.send("Route is wroking...");
 })
 
 module.exports = router;
