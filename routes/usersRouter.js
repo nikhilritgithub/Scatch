643a57e6c34073f846d552c036728e3bb255d9a6
Nikhil kumar <142686565+nikhilritgithub@users.noreply.github.com>
2025-04-24 11:31:23 +0530

    Update usersRouter.js

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function(req, res){
-    res.send("hey it's working");
+    res.send("hey it's working fine");
 })
 
-router.get("/login", function(req, res){
+router.get("/login",  function(req, res){
     res.send("login.ejs");
 })
+
 module.exports = router;
