3051184cb7ab574127d573efbeeed02db7a43e8e
Nikhil kumar <142686565+nikhilritgithub@users.noreply.github.com>
2025-04-28 09:20:21 +0530

    Update owner-model.js

--- models/owner-model.js
+++ models/owner-model.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect("mongodb://127.0.0.1:27017/scatch");
 
 const ownerSchema = mongoose.Schema ({
+    
     fullname : {
         type: String,
         minLength: 3,
