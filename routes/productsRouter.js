7113990227ae17e8cd6cb5e4f86dcfba5501e8f5
Nikhil kumar <142686565+nikhilritgithub@users.noreply.github.com>
2025-05-04 10:50:09 +0530

    Update productsRouter.js

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -9,6 +9,5 @@ router.get("/new", function(req, res){
     res.send("created new route")
 })
 
-router.get("/new-function", function(req, res){
-    res.send("working fine")
+
 module.exports = router;
