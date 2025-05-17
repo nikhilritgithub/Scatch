2a4b2bd77c83e03cdd289eb21c75f731b0883f63
Nikhil kumar <142686565+nikhilritgithub@users.noreply.github.com>
2025-05-17 17:21:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
+
 app.use("/owners", ownersRouter);
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
